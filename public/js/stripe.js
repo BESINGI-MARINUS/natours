2ed27a4ef5abd15f6ade8bd801377a1fa55694d1
BESINGI MARINUS <marinusnyando@gmail.com>
2025-10-03 21:06:32 +0100

    Modified the stripe.js file

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { showAlert } from './alert';
 import Stripe from 'stripe';
 
-// let stripe;
-// if (Stripe)
-const stripe = Stripe(
-  'pk_test_51SCceP2EVZexQnETpywwYd5Qnz4kpYlvcWwWYa6zXzo59PwHJ87fn91st8j8scXSl0PDxcV3XEMd4TRiz8fex3UZ00BWA5sBuj',
-);
+let stripe;
+if (Stripe)
+  stripe = Stripe(
+    'pk_test_51SCceP2EVZexQnETpywwYd5Qnz4kpYlvcWwWYa6zXzo59PwHJ87fn91st8j8scXSl0PDxcV3XEMd4TRiz8fex3UZ00BWA5sBuj',
+  );
 
 export const bookTour = async (tourId) => {
   try {
