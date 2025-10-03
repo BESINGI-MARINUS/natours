6efb75b7b5b9bce1dfefe66be736c3e66032028a
BESINGI MARINUS <marinusnyando@gmail.com>
2025-10-03 20:36:39 +0100

    Modified the tour template and the stripe.js in order to try and fix the 'Stripe is not defined' error

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { showAlert } from './alert';
+import Stripe from 'stripe';
 
+// let stripe;
+// if (Stripe)
 const stripe = Stripe(
   'pk_test_51SCceP2EVZexQnETpywwYd5Qnz4kpYlvcWwWYa6zXzo59PwHJ87fn91st8j8scXSl0PDxcV3XEMd4TRiz8fex3UZ00BWA5sBuj',
 );
